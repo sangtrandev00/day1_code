60c24079ea5ebcd4f885c930168b1f72dca9bf46
sangtrandev00 <nhatsang0101@gmail.com>
2022-07-24 08:37:33 +0700

    Continue

---
+++ src/day1_code_backend/challenge1.mo
@@ -0,0 +1,7 @@
+actor challenge1 {
+
+    public func add(m: Nat, n: Nat): async Nat {
+        return m + n;
+    }
+
+}
---
+++ src/day1_code_frontend/src/index.js
@@ -0,0 +1,19 @@
+import { day1_code_backend } from "../../declarations/day1_code_backend";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await day1_code_backend.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
